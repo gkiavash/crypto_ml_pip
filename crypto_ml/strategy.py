5bb8bfd8f91c83d4296ea761b2aaf202da708f71
gkiavash <gkiavash@gmail.com>
2022-10-09 10:52:54 +0200

    stop loss

--- crypto_ml/strategy.py
+++ crypto_ml/strategy.py
@@ -19,6 +19,7 @@ class TestStrategy:
                  model=None,
                  prob=0.5,
                  n_past=4,
+                 btc_qty=0.001,
                  wallet_usdt=1000,
                  wallet_btc=0,
                  rate_fee_transaction=0.001,
@@ -38,6 +39,7 @@ class TestStrategy:
         self.prob = prob
         self.n_past = n_past
 
+        self.btc_qty = btc_qty
         self.wallet_usdt = wallet_usdt
         self.wallet_btc = wallet_btc
         self.rate_fee_transaction = rate_fee_transaction
@@ -45,6 +47,7 @@ class TestStrategy:
         self.rate_sell_profit = rate_sell_profit
 
         self.positions = []
+        self.current_state = []
 
     def btc_buy(self, btc_price, btc_qty=0.0001):
         if (self.wallet_usdt - btc_qty * btc_price) < 0:
@@ -67,17 +70,25 @@ class TestStrategy:
         self.wallet_btc -= btc_qty
         self.wallet_usdt += ((btc_qty * btc_price_cell) * (1 - self.rate_fee_transaction))
         position["btc_price_cell"] = btc_price_cell
+
+        self.current_state.append(position["btc_price"] < position['btc_price_cell'])
+        if len(self.current_state) > 3:
+            self.current_state.pop(0)
         print(f'Sold {btc_qty} BTC with price {btc_price_cell}')
 
     def positions_check(self, btc_price):
         for index, position in enumerate(self.positions):
             if "btc_price_cell" in position.keys():
                 continue
-            if utils.percent_calc(btc_price, position['btc_price']) >= self.rate_sell_profit \
-                or utils.percent_calc(btc_price, position['btc_price']) < (self.rate_stop_limit - 2 * self.rate_fee_transaction):
+            if utils.percent_calc(btc_price, position['btc_price']) >= self.rate_sell_profit:
                 self.btc_sell(position, btc_price_cell=btc_price)
+            elif utils.percent_calc(btc_price, position['btc_price']) < (self.rate_stop_limit - 2 * self.rate_fee_transaction):
+                self.btc_sell(
+                    position,
+                    btc_price_cell=position["btc_price"]*(1+self.rate_stop_limit)
+                )
 
-    def run_(self, new_row):
+    def run_(self, new_row, to_buy=True):
         self.df_raw.loc[len(self.df_raw.index)] = new_row
         self.df_indicator = self.df_raw.copy(deep=True)
 
@@ -100,8 +111,12 @@ class TestStrategy:
 
         btc_price = self.df_raw.iloc[-1]["close"]
         # if y_hat_strategy_classes[0] == 1:
-        if y_prob_strategy > self.prob:
-            self.btc_buy(btc_price=btc_price)
+        if to_buy:  # End of test dataset
+            if any(self.current_state) or len(self.current_state) == 0:  # not buy if a lot of loss
+                if y_prob_strategy > self.prob and y_hat_strategy[0][1] > 0.7:
+                    self.btc_buy(btc_price=btc_price, btc_qty=self.btc_qty)
+                    self.current_state.pop(0) if len(self.current_state) > 0 else None
+
         self.positions_check(btc_price=btc_price)
 
         return y_hat_strategy, y_hat_strategy_classes
